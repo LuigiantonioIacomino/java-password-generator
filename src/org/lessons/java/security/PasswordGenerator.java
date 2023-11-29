90e8f355a346f5566f910bc547aada5b6ec2a036
LuigiantonioIacomino <iacominoluigi54@gmail.com>
2023-11-29 16:14:24 +0100

    aggiunti gli input

---
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -0,0 +1,23 @@
+package org.lessons.java.security;
+
+import java.util.Scanner;
+
+public class PasswordGenerator {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Inserisci il nome: ");
+        String name=scan.nextLine();
+        System.out.println("Inserisci il cognome: ");
+        String surname=scan.nextLine();
+        System.out.println("Inserisci il colore preferito: ");
+        String FavouriteColor=scan.nextLine();
+        System.out.println("Inserisci il giorno di nascita(numero): ");
+        int dayOfBirth=scan.nextInt();
+        System.out.println("Inserisci il mese di nascita(numero): ");
+        int monthOfBirth=scan.nextInt();
+        System.out.println("Inserisci l'anno di nascita(numero): ");
+        int yearOfBirth=scan.nextInt();
+
+
+    }
+}
